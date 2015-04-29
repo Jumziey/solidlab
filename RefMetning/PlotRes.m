97ca24649c2eca4bdd16bdc9f06e07b427b957c3
Tobias Nilsson <tobias.nilsson.garga@gmail.com>
2015-04-29 15:48:24 +0200

    Changes to plotting script and various fixes

--- RefMetning/PlotRes.m
+++ RefMetning/PlotRes.m
@@ -9,7 +9,38 @@ t1 = load('SSPTEMP1.DAT');
 t2 = load('SSPTEMP2.DAT');
 test = load('TestMeasurmentUP.dat');
 test2 = load('supercondTest2.txt');
+test3 = load('../measurements/superCondTest3Avg10delay04.dat');
+test4 = load('../measurements/superCond3Avg20delay02');
+test5 = load('../measurements/supercondAvg40delay00.dat');
+%test6 = load('../measurements/superCAvg5delay02');
+load('test6');
+%test7 = load('../measurements/superCAvg5delay02Rev.dat');
+load('test7');
 
+Revcur=[mean([test6(:,1)';test7(:,1)'])',mean([test6(:,2)';-test7(:,2)'])'];
+
+
+figure(2)
+ hold all   
+%axis([0 130 ])
+
+    plot(t1,r1)
+if 0
+    plot(test(:,1),test(:,4))
+    plot(test2(:,1),test2(:,2))
+    plot(test3(:,1),test3(:,2))
+    plot(test4(:,1),test4(:,2))
+    plot(test5(:,1),test5(:,2))
+end    
+    plot(test6(:,1),test6(:,2))
+    plot(test7(:,1),test7(:,2))
+    plot([0 120], [0 0])
+    plot(Revcur(:,1),Revcur(:,2))
+    xlabel('T [K]')
+    ylabel('Resistance, [Ohm]')
+    title('Super cond,R1 vs vs Temperature')
+
+if 0
 figure(1)
 subplot(2,2,1)
 hold on
@@ -47,3 +78,4 @@ figure(2)
     xlabel('T [K]')
     ylabel('Resistance, [Ohm]')
     title('Super cond,R1 vs vs Temperature')
+end
